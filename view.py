3daccd87e790fc09de0fe878e8156ca96f7da7f2
zhuko <zhukov_ai@outlook.com>
2024-08-19 12:23:22 +0300

    чунга чанга весело живем

--- view.py
+++ view.py
@@ -1,4 +1,4 @@
-import pygame, model,random
+import pygame, model, random
 from pygame import *
 
 display = display.set_mode([1000, 1000])
@@ -7,22 +7,25 @@ init()
 font = pygame.font.SysFont("arial", 27, True)
 
 randomniy_y_spisok = []
-randomniy_y = random.randint(10,990)
-kol_tochki = random.randint(5,15)
+randomniy_y = random.randint(10, 990)
+kol_tochki = random.randint(5, 15)
 s = range(kol_tochki)
 
-for kol_tochki in s:
+for a in s:
     randomniy_y_spisok.append(random.randint(10, 700))
 
+
 def risovanie():
-    global number_tochek,kol_tochki
     number_tochek = 0
     display.fill([0, 0, 0])
 
     while True:
-        draw.circle(display, [100,100,100], [1000 / (kol_tochki-1) * number_tochek, randomniy_y_spisok[number_tochek]], 5)
+        randomniy_x = 1000 / (kol_tochki - 1) * number_tochek
+        if number_tochek < kol_tochki-1:
+            draw.line(display, [34, 175, 41], [randomniy_x, randomniy_y_spisok[number_tochek]],
+                    [randomniy_x, randomniy_y_spisok[number_tochek + 1]],5)
         number_tochek += 1
+
         if number_tochek == kol_tochki:
             break
-    print(randomniy_y_spisok)
     pygame.display.flip()
