4f01d326cc5218e4c62c58499201cbaec7cfc0e5
zhuko <zhukov_ai@outlook.com>
2024-08-26 11:48:55 +0300

    чунга чанга весело живем

--- controller.py
+++ controller.py
@@ -3,8 +3,11 @@ from pygame import *
 pygame.key.set_repeat(50)
 timer_grafika = pygame.event.custom_type()
 pygame.time.set_timer(timer_grafika, 100)
-
-
+random_append = model.randomniy_y_spisok[-1]
+posledniy_el = model.randomniy_y_spisok[-1]
+d = 50
+e = 10
+#TODO: График вышел вверх на столько скольки равен последний элемент списка, Схема сдвига графика: все точки сдвинуть на столько на сколько вышел график
 def allsobitiya():
     global random_append
     s = pygame.event.get()
@@ -15,6 +18,6 @@ def allsobitiya():
         if a.type == timer_grafika:
             del model.randomniy_y_spisok[0]
             model.randomniy_y_spisok.append(random_append)
-            b = random_append - 50
-            c = random_append + 50
+            b = random_append - d
+            c = random_append - e
             random_append = random.randint(b, c)
