ed4eafe552317722a716c105605fec7c4dd5d2ad
zhuko <zhukov_ai@outlook.com>
2024-09-12 17:18:12 +0300

    чунга чанга весело живем

--- model.py
+++ model.py
@@ -19,15 +19,39 @@ def model_in_controller():
     global random_append
     del randomniy_y_spisok[0]
     randomniy_y_spisok.append(random_append)
-    random_ogranichenie1 = random_append - 50
-    random_ogranichenie2 = random_append + 50
-    random_append = random.randint(random_ogranichenie1, random_ogranichenie2)
+    random_append = randomniy_y_spisok[-1]
 
-    posledniy_el = randomniy_y_spisok[-1]
-    if posledniy_el < 0:
-        posledniy_el_plus = -posledniy_el
+    #Верх
+    if random_append <= 0:
+        random_ogranichenie1 = 0 - 50
+        random_ogranichenie2 = 0 + 50
+        random_append = random.randint(random_ogranichenie1, random_ogranichenie2)
+
+    #Посередине
+    if random_append > 0:
+        random_ogranichenie1 = random_append - 50
+        random_ogranichenie2 = random_append + 50
+        random_append = random.randint(random_ogranichenie1, random_ogranichenie2)
+
+    #Низ
+    if random_append > 1000:
+        random_ogranichenie1 = 1000 - 50
+        random_ogranichenie2 = 1000 + 50
+        random_append = random.randint(random_ogranichenie1, random_ogranichenie2)
+
+    posledniy_el_minus = randomniy_y_spisok[-1]
+    if posledniy_el_minus < 0:
+        posledniy_el_minus1 = -posledniy_el_minus
         dlina_spiska = len(randomniy_y_spisok)
         for i in range(0, dlina_spiska):
-            randomniy_y_spisok[i] += posledniy_el_plus
+            randomniy_y_spisok[i] += posledniy_el_minus1
+
+    posledniy_el_minus = randomniy_y_spisok[-1]
+    if posledniy_el_minus > 1000:
+        dlina_spiska = len(randomniy_y_spisok)
+        for i in range(0, dlina_spiska):
+            randomniy_y_spisok[i] -= posledniy_el_minus-1000
     print(randomniy_y_spisok[-1],random_append)
+
+
 
